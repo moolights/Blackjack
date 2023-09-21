4dec718b9119185d847ee0f431c023b737674b73
Wyatt Boublil <wiggles_worth@outlook.com>
2023-09-20 17:14:21 -0700

    Refactor to code

--- dealer.py
+++ dealer.py
@@ -3,8 +3,39 @@ import time
 
 INIT_HAND = 2
 
-def get_deck():
-    deck = []
+class Deck:
+    cards = []
+    
+    def __init__(self, cards):
+        self.cards = cards
+    
+    def get_cards(self):
+        return self.cards
+    
+    def shuffle(self):
+        print("\nLet's shuffle the deck...\n")
+        time.sleep(1)
+        n = len(self.cards)
+        for i in range(n - 1, 0, -1):
+            j = random.randint(0, i)
+            self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
+            print(self.cards[i], end= " ")
+            time.sleep(.05)
+            if i % 13 == 0:
+                print("\n")
+    
+        print(f"\n\nTotal card count: {len(self.cards)}")
+    
+    def initial_deal(self):
+        starting_cards = []
+        for _ in range(0, INIT_HAND):
+            card = random.choice(self.cards)
+            starting_cards.append(card)
+            self.cards.remove(card)
+        return starting_cards
+
+def create_deck():
+    cards = []
     ranks = {
         "A" : 4,
         "2" : 4,
@@ -21,36 +52,35 @@ def get_deck():
         "K" : 4,
     }
 
-    # for card, card_count in ranks.items():
-    #   for _ in range(card_count):
-    #       deck.append(card)
+    cards = [card for card, card_count in ranks.items() for _ in range(card_count)]
     
-    deck = [card for card, card_count in ranks.items() for _ in range(card_count)]
-    return deck 
+    deck = Deck(cards)
+    return deck
 
 # Displays the shuffling deck to console using Fisher-Yates Algorithm
-def shuffle(deck):
-    print("\nLet's get started. First, let's shuffle the deck...\n")
-    time.sleep(1)
-    n = len(deck)
-    for i in range(n - 1, 0, -1):
-        j = random.randint(0, i)
-        deck[i], deck[j] = deck[j], deck[i]
-        print(deck[i], end= " ")
-        time.sleep(.05)
-        if i % 13 == 0:
-            print("\n")
+# def shuffle(deck):
+#     print("\nLet's get started. First, let's shuffle the deck...\n")
+#     time.sleep(1)
+#     n = len(deck)
+#     for i in range(n - 1, 0, -1):
+#         j = random.randint(0, i)
+#         deck[i], deck[j] = deck[j], deck[i]
+#         print(deck[i], end= " ")
+#         time.sleep(.05)
+#         if i % 13 == 0:
+#             print("\n")
     
-    shuffled_deck = deck
-    print(f"\n\nTotal card count: {len(shuffled_deck)}")
-    return shuffled_deck
+#     shuffled_deck = deck
+#     print(f"\n\nTotal card count: {len(shuffled_deck)}")
+#     return shuffled_deck
 
-def initial_deal(deck):
-    starting_cards = []
-    for _ in range(0, INIT_HAND):
-        card = random.choice(deck)
-        starting_cards.append(card)
-        deck.remove(card)
-    return starting_cards   
+# def initial_deal(deck):
+#     starting_cards = []
+#     for _ in range(0, INIT_HAND):
+#         card = random.choice(deck)
+#         starting_cards.append(card)
+#         deck.remove(card)
+#     return starting_cards   
 
 # Need function to deal cards when hitting********************
+
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import time
 import cardvalues
-import dealer
-from dealer import get_deck
+from dealer import create_deck
 
 MAX_BET = 100
 MIN_BET = 1
-INIT_HAND = 2
 BLACKJACK = 21
 
 class Player:
@@ -14,10 +12,9 @@ class Player:
     hand_total  =  0
     ace_in_hand = False
 
-    def __init__(self, cards, balance= 0, hand_total= 0):
+    def __init__(self, cards = None, balance= 0):
         self.cards = cards
         self.balance = balance
-        self.hand_total = hand_total
 
     def has_bust(self):
         return self.hand_total > BLACKJACK
@@ -29,83 +26,64 @@ class Player:
             return cardvalues.CARDS["Ace_Eleven"]
         else: 
             return cardvalues.CARDS["Ace_One"]
-
-# Maybe move this and display_cards() to the player class
-def hand_total(player):
-    hand_total = 0
-    for card in player.cards:
-        if card == "A":
-            hand_total += player.set_ace()
-        else:
-            hand_total += cardvalues.CARDS[card]
     
-    if player.has_bust():
-        print("You busted!")
-          
-    return hand_total
-    
-def display_cards(player):
-    print("\nNow lets deal your cards...\n")
-    time.sleep(1)
-    for card in player.cards:
-        print(f"[{card}]", end= " ")
-        time.sleep(.8)
-    print("\n")
-    
-# Need to ask user for addition deposits if balance is insufficient or user just wants to keep going****************
-def deposit():
-    while True:
-        amount = input("\n\nHow much would you like to deposit? $")
-        if amount.isdigit(): # For some reason negative values aren't registering
-            amount = int(amount)
-            if amount > 0:
-                break
+    def value(self):
+        for card in self.cards:
+            if card == "A":
+                self.hand_total += self.set_ace()
             else:
-                print("Amount must be greater than $0")
-        else:
-            print("Please enter a number")
-            
-    return amount
-
-def get_bet(player):
-    while True:
-        bet = input("\nHow much would you like to bet? $")
-        if bet.isdigit():
-            bet = int(bet)
-            if bet > player.balance:
-                print(f"${bet} is greater than your balance of ${player.balance}")
-            elif MIN_BET <= bet <= MAX_BET:
-                break
+                self.hand_total += cardvalues.CARDS[card]
+        if self.has_bust():
+            print("You busted!")
+        print(f"Total: {self.hand_total}")
+        
+    def display_cards(self):
+        print("\nNow lets deal your cards...\n")
+        time.sleep(1)
+        for card in self.cards:
+            print(f"[{card}]", end= " ")
+            time.sleep(.8)
+        print("\n")
+    
+    # Need to ask user for addition deposits if balance is insufficient or user just wants to keep going****************
+    def deposit(self):
+        while True:
+            amount = input("\n\nHow much would you like to deposit? $")
+            if amount.isdigit(): # For some reason negative values aren't registering
+                amount = int(amount)
+                if amount > 0:
+                    break
+                else:
+                    print("Amount must be greater than $0")
             else:
-                print(f"Your bet must be between ${MIN_BET} - ${MAX_BET}")
-        else:
-            print("Please enter a number...")
-    
-    return bet
+                print("Please enter a number") 
+        self.balance += amount
+        
+    def bet(self):
+        while True:
+            bet = input("\nHow much would you like to bet? $")
+            if bet.isdigit():
+                bet = int(bet)
+                if bet > self.balance:
+                    print(f"${bet} is greater than your balance of ${self.balance}")
+                elif MIN_BET <= bet <= MAX_BET:
+                    break
+                else:
+                    print(f"Your bet must be between ${MIN_BET} - ${MAX_BET}")
+            else:
+                print("Please enter a number...")
+        self.balance -= bet
 
-def start():
-    print("\nWelcome to Blackjack!")
-    balance = deposit()
-    
-    deck = dealer.shuffle(get_deck())
-    
-    starting_deal = dealer.initial_deal(deck)
-    temp_hand = Player(starting_deal)
-    
-    # This will change if I move hand_total and display_cards to class
-    player = Player(starting_deal, balance, hand_total(temp_hand))
-    
-    bet = get_bet(player)
-    player.balance -= bet
-    
-    display_cards(player)
-    print(f"Total: {player.hand_total}")
-    
-    return player
-    
 def main():
-    player = start()
+    print("\nWelcome to Blackjack!")
+    player = Player()
+    player.deposit()
+    player.bet()
+    deck = create_deck()
+    deck.shuffle()
+    player.cards = deck.initial_deal()
+    player.display_cards()
+    player.value()
     
-
 if __name__ == "__main__":
     main()
